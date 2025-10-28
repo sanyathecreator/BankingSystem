82a2722f80e22bbe94ed5e93845222a07ed98359
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-10-28 20:28:33 +0100

    feat: add transactions logging functionality

--- app/src/main/java/org/example/repository/TransactionRepository.java
+++ app/src/main/java/org/example/repository/TransactionRepository.java
@@ -1,8 +1,10 @@
 package org.example.repository;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -13,6 +15,7 @@ import org.example.model.Transaction;
 
 public class TransactionRepository {
     private static final String DATA_FILE = "transactions.dat";
+    private static final String LOGS_FILE = "logs.txt";
     private List<Transaction> transactions;
 
     public TransactionRepository() {
@@ -27,6 +30,17 @@ public class TransactionRepository {
     public void saveTransaction(Transaction transaction) {
         transactions.add(transaction);
         saveTransactions();
+    }
+
+    public void logTransactions() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOGS_FILE))) {
+            for (Transaction transaction : transactions) {
+                writer.write(transaction.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Cannot write transactions to log file. Error: " + e.getMessage());
+        }
     }
 
     @SuppressWarnings("unchecked")
