6391cca4376ee194d4ce0ea5d7c02ca22052b24f
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-10-29 19:30:18 +0100

    feat: add transaction validation functionality

--- app/src/main/java/org/example/service/TransactionService.java
+++ app/src/main/java/org/example/service/TransactionService.java
@@ -15,34 +15,56 @@ public class TransactionService {
         this.userRepository = userRepository;
     }
 
-    public boolean makeTransaction(String senderUsername, String receiverUsername, double amount) {
-        if (!validateCustomer(senderUsername) || !validateCustomer(receiverUsername)) {
-            System.out.println("Customer doesn't exixst!");
-            return false;
-        }
+    public TransactionStatus makeTransaction(String senderUsername, String receiverUsername, double amount) {
+        TransactionStatus validation = validateTransaction(senderUsername, receiverUsername, amount);
+        if (validation != TransactionStatus.SUCCESS)
+            return validation;
         Customer sender = (Customer) userRepository.getUser(senderUsername);
         Customer receiver = (Customer) userRepository.getUser(receiverUsername);
 
-        if (!isEnoughMoney(sender, amount)) {
-            System.out.println("Customer doesn't have enough money!");
-            return false;
-        }
         sender.setBalance(sender.getBalance() - amount);
         receiver.setBalance(receiver.getBalance() + amount);
+        userRepository.saveUser(sender);
+        userRepository.saveUser(receiver);
 
         transactionRepository.saveTransaction(new Transaction(senderUsername, receiverUsername, amount));
-        return true;
+        return TransactionStatus.SUCCESS;
     }
 
-    private boolean isEnoughMoney(Customer customer, double amount) {
-        return customer.getBalance() >= amount;
+    private TransactionStatus validateFunds(Customer customer, double amount) {
+        if (amount <= 0)
+            return TransactionStatus.INVALID_AMOUNT;
+        if (customer.getBalance() < amount)
+            return TransactionStatus.NOT_ENOUGH_MONEY;
+        return TransactionStatus.SUCCESS;
     }
 
-    private boolean validateCustomer(String username) {
-        if (!userRepository.userExists(username)) {
-            return false;
-        }
+    private TransactionStatus validateTransaction(String senderUsername, String receiverUsername, double amount) {
+        TransactionStatus senderValidation = validateUser(senderUsername);
+        TransactionStatus receiverValidation = validateUser(receiverUsername);
+        if (senderValidation != TransactionStatus.SUCCESS)
+            return senderValidation;
+        if (receiverValidation != TransactionStatus.SUCCESS)
+            return receiverValidation;
+        if (senderUsername.equals(receiverUsername))
+            return TransactionStatus.SAME_ACCOUNT;
+
+        Customer sender = (Customer) userRepository.getUser(senderUsername);
+        TransactionStatus moneyAmountValidation = validateFunds(sender, amount);
+        if (moneyAmountValidation != TransactionStatus.SUCCESS)
+            return moneyAmountValidation;
+
+        return TransactionStatus.SUCCESS;
+    }
+
+    private TransactionStatus validateUser(String username) {
         User user = userRepository.getUser(username);
-        return user instanceof Customer;
+        if (user == null) {
+            return TransactionStatus.CUSTOMER_NOT_FOUND;
+        }
+        if (!(user instanceof Customer)) {
+            return TransactionStatus.CUSTOMER_WRONG_TYPE;
+        }
+        return TransactionStatus.SUCCESS;
     }
 }
---
+++ app/src/main/java/org/example/service/TransactionStatus.java
@@ -0,0 +1,10 @@
+package org.example.service;
+
+public enum TransactionStatus {
+    SUCCESS,
+    CUSTOMER_NOT_FOUND,
+    CUSTOMER_WRONG_TYPE,
+    NOT_ENOUGH_MONEY,
+    INVALID_AMOUNT,
+    SAME_ACCOUNT
+}
