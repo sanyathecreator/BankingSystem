0e5e3d5ff0a5478fe589d5d929d5873a4cb65e2b
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-11-01 16:29:01 +0100

    feat: add customer transactions view functionality

--- app/src/main/java/org/example/service/CustomerService.java
+++ app/src/main/java/org/example/service/CustomerService.java
@@ -1,13 +1,20 @@
 package org.example.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.example.model.Customer;
+import org.example.model.Transaction;
+import org.example.repository.TransactionRepository;
 import org.example.repository.UserRepository;
 
 public class CustomerService {
     private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
 
-    public CustomerService(UserRepository userRepository) {
+    public CustomerService(UserRepository userRepository, TransactionRepository transactionRepository) {
         this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
     }
 
     public void checkBalance(Customer customer) {
@@ -24,5 +31,18 @@ public class CustomerService {
                 "Transferred: $" + String.format("%.2f", amount) +
                         "\nYour balance: $" + String.format("%.2f", customer.getBalance()));
         userRepository.saveUser(customer);
+    }
+
+    public List<Transaction> getTransactionsHistory(Customer customer) {
+        List<Transaction> allTransactions = transactionRepository.getAllTransactions();
+        List<Transaction> customerTransactions = new ArrayList<>();
+        for (Transaction transaction : allTransactions) {
+            if (transaction.getSenderUsername().equals(customer.getUsername())
+                    || transaction.getReceiverUsername().equals(customer.getUsername())) {
+                customerTransactions.add(transaction);
+            }
+        }
+
+        return customerTransactions;
     }
 }
